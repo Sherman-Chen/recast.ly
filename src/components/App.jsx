7a1b1d258c657c80b1fcb088bca4c8f989d7635c
Sherman-Chen <shc410@gmail.com>
2016-09-30 16:34:14 -0700

    Refactor App to class + stateful component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      currentVideo: window.exampleVideoData[0],
+      videoList: window.exampleVideoData
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Nav />
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo}/>
+          </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videoList}/>
+        </div>
+      </div>
+    );
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
