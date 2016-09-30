f699bbf902b9364ecae60c104899fb64629e23de
Sherman-Chen <shc410@gmail.com>
2016-09-30 15:33:26 -0700

    Setup Sprint and pass tests for VideoList && VideoListEntry

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,14 +1,19 @@
-var VideoListEntry = () => (
+var VideoListEntry = (props) => {
+
+  var attributes = props.video.snippet;
+  
+  return (
   <div className="video-list-entry">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
+      <img className="media-object" src={attributes.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
-      <div className="video-list-entry-title">Video Title</div>
-      <div className="video-list-entry-detail">Video Description</div>
+      <div className="video-list-entry-title">{attributes.title}</div>
+      <div className="video-list-entry-detail">{attributes.description}</div>
     </div>
   </div>
-);
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
